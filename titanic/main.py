2cefa5702ecb1bdeea73a65f4290177ecf9554ff
astridhult <astridhult4@gmail.com>
2023-10-22 19:51:15 +0200

    Messed around with regualrization

--- titanic/main.py
+++ titanic/main.py
@@ -5,7 +5,8 @@ import datetime as dt
 import numpy as np
 import random
 import statsmodels.api as sm
-from patsy import dmatrices
+from xgboost import XGBClassifier
+from patsy import dmatrices, dmatrix
 
 def print_data_frame(df: pl.DataFrame):
     column_config = {}
@@ -18,6 +19,9 @@ def print_data_frame(df: pl.DataFrame):
                 col_name, format="YYYY-MM-DD HH:mm:ss.SSS"
             )
     st.dataframe(df, column_config=column_config)
+
+# def modify_df(df: pl.DataFrame):
+
 
 st.write("Astrids titanic app")
 
@@ -33,28 +37,28 @@ df = df.with_columns(
     ).alias("Class Sex"),
     )
 
-print_data_frame(df)
+
+df = df.with_columns(
+    pl.when(pl.col('Age')>6).then(0).otherwise(pl.col('Age')).fill_null(strategy="zero").alias("Age_kids")
+)
+
+# print_data_frame(df)
 
 pandas_df = df.to_pandas()
 
-# Add a constant term to x
-Fares_with_const = sm.add_constant(pandas_df['Fare'])
+y, X = dmatrices('Survived ~ Fare + Age + Sex + Pclass + SibSp + Parch + Embarked', data=pandas_df, return_type='dataframe')
 
 # Fit the model
-model = sm.OLS(pandas_df['Survived'], Fares_with_const)
-results = model.fit()
-st.write(results.summary())
+model = sm.OLS(y, X)
+results = model.fit_regularized(alpha=0.5, L1_wt=0.0)
+# st.write(results.summary())
 
 coefficients = np.array(results.params)
-Survived_fitted = Fares_with_const @ coefficients
+Survived_fitted = X @ coefficients
 
 pandas_df['Survived_fitted'] = Survived_fitted
 
-# st.dataframe(pandas_df)
-
-# Survived_measured = df['Survived']
-
-# df_linreg = pl.DataFrame({'Fares': Fares_with_const, 'Survived': Survived_measured , 'Survived_fitted': Survived_fitted})
+st.dataframe(pandas_df)
 
 scatter = (
     alt.Chart(pandas_df).mark_point()
@@ -75,40 +79,59 @@ line = (
 chart4 = scatter + line
 st.altair_chart(chart4, use_container_width=True)
 
-# color_scale = alt.Scale(domain=[0, 1], range=['red', 'green'])
+#Modeling the testdata
+df_test = pl.read_csv('data/test.csv')
 
-# chart1 = (
-#     alt.Chart(df).mark_point()
-#     .encode(
-#         x="Pclass:O",
-#         y='Age:Q',
-#         color=alt.Color("Survived:N", scale=color_scale),
-#         tooltip = ['PassengerId','Survived','Pclass','Name','Sex','Age','SibSp','Parch','Ticket','Fare','Cabin','Embarked' ]
-#     )
-# )
-# st.altair_chart(chart1, use_container_width=True)
+df_test = df_test.with_columns(
+    pl.when(pl.col('Age')>6).then(0).otherwise(pl.col('Age')).fill_null(strategy="zero").alias("Age_kids")
+)
 
-# chart2 = (
-#     alt.Chart(df).mark_point()
-#     .encode(
-#         x="Class Sex:N",
-#         y='Fare:Q',
-#         color=alt.Color("Survived:N", scale=color_scale),
-#         tooltip = ['PassengerId','Survived','Pclass','Name','Sex','Age','SibSp','Parch','Ticket','Fare','Cabin','Embarked' ]
-#     )
-# )
-# st.altair_chart(chart2, use_container_width=True)
+pandas_df_test = df_test.to_pandas()
+X_test = dmatrix('Fare + Age + Sex + Pclass + SibSp + Parch + Embarked ', data=pandas_df_test, return_type='dataframe')
+Survived_test = ((X_test @ coefficients)>0.6)
 
-# chart3 = (
-#     alt.Chart(df).mark_point()
-#     .encode(
-#         x="Fare:Q",
-#         y='Survived:N',
-#         color=alt.Color("Survived:N", scale=color_scale),
-#         tooltip = ['PassengerId','Survived','Pclass','Name','Sex','Age','SibSp','Parch','Ticket','Fare','Cabin','Embarked' ]
-#     )
-# )
-# st.altair_chart(chart3, use_container_width=True)
+
+st.write(Survived_test)
+
+pandas_df_test['Survived_test'] = Survived_test
+pandas_df_test['Survived_test'] =pandas_df_test['Survived_test'].fillna(False)
+pandas_df_test['Survived_test'] =pandas_df_test['Survived_test'].astype(int)
+
+pandas_df_test[['PassengerId','Survived_test']].to_csv('answer.csv', index=False)
+color_scale = alt.Scale(domain=[0, 1], range=['red', 'green'])
+
+chart1 = (
+    alt.Chart(df).mark_point()
+    .encode(
+        x="Pclass:O",
+        y='Age:Q',
+        color=alt.Color("Survived:N", scale=color_scale),
+        tooltip = ['PassengerId','Survived','Pclass','Name','Sex','Age','SibSp','Parch','Ticket','Fare','Cabin','Embarked' ]
+    )
+)
+st.altair_chart(chart1, use_container_width=True)
+
+chart2 = (
+    alt.Chart(df).mark_point()
+    .encode(
+        x="Class Sex:N",
+        y='Age:Q',
+        color=alt.Color("Survived:N", scale=color_scale),
+        tooltip = ['PassengerId','Survived','Pclass','Name','Sex','Age','SibSp','Parch','Ticket','Fare','Cabin','Embarked' ]
+    )
+)
+st.altair_chart(chart2, use_container_width=True)
+
+chart3 = (
+    alt.Chart(df).mark_bar()
+    .encode(
+        x=alt.X('Age:Q', bin=alt.Bin(step=3), title='Age'),
+        y='count()',
+        color=alt.Color("Survived:N", scale=color_scale),
+        tooltip = ['PassengerId','Survived','Pclass','Name','Sex','Age','SibSp','Parch','Ticket','Fare','Cabin','Embarked' ]
+    )
+)
+st.altair_chart(chart3, use_container_width=True)
 
 
 
