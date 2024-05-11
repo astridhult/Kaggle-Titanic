893c4094a42432ff7258b1b76a761a4af368d17d
astridhult <astridhult4@gmail.com>
2024-05-11 21:49:09 +0200

    Save work

--- titanic/main.py
+++ titanic/main.py
@@ -6,7 +6,10 @@ import numpy as np
 import random
 import statsmodels.api as sm
 from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split
 from patsy import dmatrices, dmatrix
+from rich.traceback import install
+install()
 
 def print_data_frame(df: pl.DataFrame):
     column_config = {}
@@ -20,7 +23,7 @@ def print_data_frame(df: pl.DataFrame):
             )
     st.dataframe(df, column_config=column_config)
 
-# def modify_df(df: pl.DataFrame):
+# # def modify_df(df: pl.DataFrame):
 
 
 st.write("Astrids titanic app")
@@ -46,94 +49,119 @@ df = df.with_columns(
 
 pandas_df = df.to_pandas()
 
-y, X = dmatrices('Survived ~ Fare + Age + Sex + Pclass + SibSp + Parch + Embarked', data=pandas_df, return_type='dataframe')
+gender_mapping = {'male': 1, 'female': 0}
+
+# Use the map method to convert the 'gender' column
+pandas_df['Sex'] = pandas_df['Sex'].map(gender_mapping)
+
+X = pandas_df[['Fare', 'Age', 'Sex', 'Pclass']]
+y = pandas_df['Survived']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.001, random_state=42)
+
+# st.write(X_train)
+# y, X = dmatrices('Survived ~ Fare + Age + Sex + Pclass + SibSp + Parch + Embarked', data=pandas_df, return_type='dataframe')
+# X.columns = X.columns.str.replace(r'[\[\],\.]', '_', regex=True)
 
 # Fit the model
-model = sm.OLS(y, X)
-results = model.fit_regularized(alpha=0.5, L1_wt=0.0)
-# st.write(results.summary())
+# model = sm.OLS(y, X)
 
-coefficients = np.array(results.params)
-Survived_fitted = X @ coefficients
+XGmodel = XGBClassifier(n_estimators=3, max_depth=3, learning_rate=0.1, objective='binary:logistic')
 
-pandas_df['Survived_fitted'] = Survived_fitted
+results = XGmodel.fit(X_train, y_train) 
 
-st.dataframe(pandas_df)
+st.write(results)
+print(results)
+# coefficients = np.array(results.params)
+# Survived_fitted = X @ coefficients
 
-scatter = (
-    alt.Chart(pandas_df).mark_point()
-    .encode(
-        x='Fare:Q',
-        y='Survived:Q'
-    )
-)
+# pandas_df['Survived_fitted'] = Survived_fitted
 
-line = (
-    alt.Chart(pandas_df).mark_line()
-    .encode(
-       x='Fare:Q',
-       y='Survived_fitted:Q' 
-    )
-)
+# st.dataframe(pandas_df)
 
-chart4 = scatter + line
-st.altair_chart(chart4, use_container_width=True)
-
-#Modeling the testdata
+# #Modeling the testdata
 df_test = pl.read_csv('data/test.csv')
-
-df_test = df_test.with_columns(
-    pl.when(pl.col('Age')>6).then(0).otherwise(pl.col('Age')).fill_null(strategy="zero").alias("Age_kids")
-)
+st.write(df_test)
+# df_test = df_test.with_columns(
+#     pl.when(pl.col('Age')>6).then(0).otherwise(pl.col('Age')).fill_null(strategy="zero").alias("Age_kids")
+# )
 
 pandas_df_test = df_test.to_pandas()
-X_test = dmatrix('Fare + Age + Sex + Pclass + SibSp + Parch + Embarked ', data=pandas_df_test, return_type='dataframe')
-Survived_test = ((X_test @ coefficients)>0.6)
+
+gender_mapping = {'male': 1, 'female': 0}
+pandas_df_test['Sex'] = pandas_df_test['Sex'].map(gender_mapping)
 
 
-st.write(Survived_test)
+X_test = pandas_df_test[['Fare', 'Age', 'Sex', 'Pclass']]
+# X_test = dmatrix('Fare + Age + Sex + Pclass + SibSp + Parch + Embarked ', data=pandas_df_test, return_type='dataframe')
+# Survived_test = ((X_test @ coefficients)>0.6)
 
-pandas_df_test['Survived_test'] = Survived_test
+preds = XGmodel.predict(X_test)
+st.write(X_test)
+print(preds)
+st.write(preds)
+# scatter = (
+#     alt.Chart(pandas_df).mark_point()
+#     .encode(
+#         x='Fare:Q',
+#         y='Survived:Q'
+#     )
+# )
+
+# line = (
+#     alt.Chart(pandas_df).mark_line()
+#     .encode(
+#        x='Fare:Q',
+#        y='Survived_fitted:Q' 
+#     )
+# )
+
+# chart4 = scatter + line
+# st.altair_chart(chart4, use_container_width=True)
+
+
+
+
+# st.write(Survived_test)
+
+pandas_df_test['Survived_test'] = preds
 pandas_df_test['Survived_test'] =pandas_df_test['Survived_test'].fillna(False)
 pandas_df_test['Survived_test'] =pandas_df_test['Survived_test'].astype(int)
 
 pandas_df_test[['PassengerId','Survived_test']].to_csv('answer.csv', index=False)
-color_scale = alt.Scale(domain=[0, 1], range=['red', 'green'])
+# color_scale = alt.Scale(domain=[0, 1], range=['red', 'green'])
 
-chart1 = (
-    alt.Chart(df).mark_point()
-    .encode(
-        x="Pclass:O",
-        y='Age:Q',
-        color=alt.Color("Survived:N", scale=color_scale),
-        tooltip = ['PassengerId','Survived','Pclass','Name','Sex','Age','SibSp','Parch','Ticket','Fare','Cabin','Embarked' ]
-    )
-)
-st.altair_chart(chart1, use_container_width=True)
+# chart1 = (
+#     alt.Chart(df).mark_point()
+#     .encode(
+#         x="Pclass:O",
+#         y='Age:Q',
+#         color=alt.Color("Survived:N", scale=color_scale),
+#         tooltip = ['PassengerId','Survived','Pclass','Name','Sex','Age','SibSp','Parch','Ticket','Fare','Cabin','Embarked' ]
+#     )
+# )
+# st.altair_chart(chart1, use_container_width=True)
 
-chart2 = (
-    alt.Chart(df).mark_point()
-    .encode(
-        x="Class Sex:N",
-        y='Age:Q',
-        color=alt.Color("Survived:N", scale=color_scale),
-        tooltip = ['PassengerId','Survived','Pclass','Name','Sex','Age','SibSp','Parch','Ticket','Fare','Cabin','Embarked' ]
-    )
-)
-st.altair_chart(chart2, use_container_width=True)
+# chart2 = (
+#     alt.Chart(df).mark_point()
+#     .encode(
+#         x="Class Sex:N",
+#         y='Age:Q',
+#         color=alt.Color("Survived:N", scale=color_scale),
+#         tooltip = ['PassengerId','Survived','Pclass','Name','Sex','Age','SibSp','Parch','Ticket','Fare','Cabin','Embarked' ]
+#     )
+# )
+# st.altair_chart(chart2, use_container_width=True)
 
-chart3 = (
-    alt.Chart(df).mark_bar()
-    .encode(
-        x=alt.X('Age:Q', bin=alt.Bin(step=3), title='Age'),
-        y='count()',
-        color=alt.Color("Survived:N", scale=color_scale),
-        tooltip = ['PassengerId','Survived','Pclass','Name','Sex','Age','SibSp','Parch','Ticket','Fare','Cabin','Embarked' ]
-    )
-)
-st.altair_chart(chart3, use_container_width=True)
-
-
+# chart3 = (
+#     alt.Chart(df).mark_bar()
+#     .encode(
+#         x=alt.X('Age:Q', bin=alt.Bin(step=3), title='Age'),
+#         y='count()',
+#         color=alt.Color("Survived:N", scale=color_scale),
+#         tooltip = ['PassengerId','Survived','Pclass','Name','Sex','Age','SibSp','Parch','Ticket','Fare','Cabin','Embarked' ]
+#     )
+# )
+# st.altair_chart(chart3, use_container_width=True)
 
 
 
